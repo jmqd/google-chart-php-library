b98eb52ba8c58c135f371e0669f5be56b004a4e9
Jordan McQueen <dojo3@uw.edu>
2016-06-23 21:00:46 -0700

    adding require line

--- charts/AreaChart.php
+++ charts/AreaChart.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('../GoogleChart.php');
+
 class AreaChart extends GoogleChart {
     
     public function __construct(GoogleChart $chart) {
--- charts/BarChart.php
+++ charts/BarChart.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('../GoogleChart.php');
+
 class BarChart extends GoogleChart {
     
     public function __construct(GoogleChart $chart) {
