516eeb5b54dc55f101e404082f525f4f98b17381
Jordan McQueen <dojo3@uw.edu>
2016-06-26 11:52:48 -0700

    beginning swap of config to class

--- config/config.php
+++ config/config.php
@@ -1,17 +1,23 @@
 <?php
 
-$config = [];
+class Config {
 
-$config['annotated_dates'] = 
-[
-	'2016-01-01' => 'New Year\'s Day!',
-	'2016-11-01' => 'First of November',
-	'2016-06-23' => 'The day I wrote this config file',
-];
 
-$config['supported_options'] = [
-    'stacked',
-    'separate_axes',
-];
+    public function __construct() {
 
+        $config['annotated_dates'] = 
+        [
+            '2016-01-01' => 'New Year\'s Day!',
+            '2016-11-01' => 'First of November',
+            '2016-06-23' => 'The day I wrote this config file',
+        ];
+
+        $config['supported_options'] = [
+            'stacked',
+            'separate_axes',
+        ];
+
+}
+
+}
 ?>
