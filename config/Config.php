cb089379d38aae6a6317d71a7c214de07a031a76
Jordan McQueen <dojo3@uw.edu>
2016-06-27 00:40:42 -0700

    config stuff

---
+++ config/Config.php
@@ -0,0 +1,30 @@
+<?php
+
+class Config 
+{
+
+    public $annotated_dates;
+    public $supported_options;
+    public $default_chart;
+
+    public function __construct()
+    {
+
+        $this->annotated_dates = [
+            '2016-01-01' => 'New Year\'s Day!',
+            '2016-11-01' => 'First of November',
+            '2016-06-23' => 'The day I wrote this config file',
+            ];
+
+        $this->supported_options = [
+            'stacked',
+            'separate_axes',
+            ];
+
+        $this->default_chart = 'line';
+
+    }
+
+}
+
+?>
