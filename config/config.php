6dae30240c7714e109110b13929c164c030d7bb6
Jordan McQueen <dojo3@uw.edu>
2016-07-01 13:24:48 -0700

    the one true brace style and refactoring div style to be more modular

--- config/config.php
+++ config/config.php
@@ -2,23 +2,30 @@
 
 return 
 [
-    'annotated_dates' => [
+    'annotated_dates' => 
+    [
         '2016-01-01' => 'New Years Day!',
         '2016-11-01' => 'First of November',
         '2016-06-23' => 'The day I wrote this config file',
-        ],
+    ],
 
-    'default_settings' => [
+    'default_settings' => 
+    [
         'is_sharing_axes' => true,
         'is_stacked' => false,
         'point_size' => 0,
-        'div_style' => "style='border: 0px solid; width:1400px",
-        'kind' => 'line',
+        'div_styles' => 
+        [
+            ['style' => 'border', 'value' => '0px solid'], 
+            ['style' => 'width', 'value' => '1400px'], 
         ],
+        'kind' => 'line',
+    ],
     
     'supported_features' => ['png'],
     
-    'class_name_map' => [
+    'class_name_map' => 
+    [
  		'line' => 'LineChart',
          'bar' => 'BarChart',
          'area' => 'AreaChart',
@@ -27,15 +34,17 @@ return
          'table' => 'Table',
          'timeseries' => 'TimeseriesChart',
          'scatter' => 'ScatterChart',
-    	],
+    ],
 	
-    'default_characteristics' => [
+    'default_characteristics' => 
+    [
         'has_results' => true,
-        ],
+    ],
 
-    'default_features' => [
+    'default_features' => 
+    [
         
-        ],
+    ],
 ];
 
 ?>
